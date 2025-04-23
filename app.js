e4d4b5e53688b58d76088af623d4166762e0ae5f
Huseyn33 <huseynoyunkanali1@gmail.com>
2025-04-23 23:22:27 +0400

    finish

---
+++ app.js
@@ -0,0 +1,8 @@
+// let user1= prompt("birinci ededi daxil edin")
+// let user2= prompt("ikinci ededi daxil edin")
+// console.log(typeof user1)
+// console.log(typeof user2)
+// console.log(+user1+ +user2)
+// console.log(typeof +user1)
+// console.log(typeof +user2)
+
---
+++ app2.js
@@ -0,0 +1,91 @@
+// // let a= +prompt("bir eded daxil edin")
+
+
+// // a>=50?console.log("imtahandan kecdiniz"):console.log("kecebilmediniz");
+
+// let a= +prompt("ayin necesidi")
+// switch (a) {
+//     case 1:
+//         alert("yanvar ayidir")
+//         break;
+//     case 2:
+//         alert("fevral ayidir")
+        
+//         break;
+//     case 3:
+//         alert("mart ayidir")
+//         break;
+//     case 4:
+//         alert("aprel ayidir")
+//         break;
+//     case 5:
+//         alert("may ayidir")
+//         break;
+//     case 6:
+//         alert("iyun ayidir")
+//         break;
+//     case 7:
+//         alert("iyul ayidir")
+//         break;
+//     case 8:
+//        alert("avqust ayidir") 
+//         break;
+//     case 9:
+//         alert("sentyabr ayidir")
+//         break;
+//     case 10 :
+//         alert("oktyabr ayidir")
+//         break;
+//     case 11:
+//         alert("noyabr ayidir")
+//     case 12:
+//         alert("dekabr ayidir")
+    
+
+//     default:
+//         alert("bu ay deyil")
+//         break;
+// }
+let seher= +prompt("bir erazi adi yazin")
+switch (seher) {
+    case Cebrayil:
+        alert("04.10.2023")
+        break;
+    case Fuzuli:
+        alert("17.10.2020")
+        break;
+    case Zengilan:
+        alert("20.10.2020")
+        break;
+    case Qubadli:
+        alert("25.10.2020")
+        break;
+    case Susa:
+        alert("10.11.2020")
+        break;
+    case Xankendi:
+        alert("10.11.2020")
+        break;
+    case Xocali:
+        alert("10.11.2020")
+        break;
+    case Xocavend:
+        alert("10.11.2020")
+    break;
+    case Agdere:
+        alert("10.11.2020")
+        break;
+    case Agdam:
+        alert("20.11.2020")
+        break;
+    case Kelbecer:
+        alert("25.11.2020")
+        break;
+    case Lacin:
+        alert("01.12.2020.")
+        break;
+  
+    default:
+        alert("bu isgal edilen erazilerimizden biri deyil")
+        break;
+}
